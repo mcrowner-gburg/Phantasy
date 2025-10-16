d72f3f25ee8a0d9994cc80b4ed377c5ec6556d5d
mcrowner-gburg <mcrowner@barkingcatmediagroup.com>
2025-10-16 12:48:34 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,12 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<App />);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
