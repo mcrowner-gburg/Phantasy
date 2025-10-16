578a67faa1e815f9ec4dfa6a9b68f14fec7c92cf
mcrowner-gburg <mcrowner@barkingcatmediagroup.com>
2025-10-16 16:21:34 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,5 +88,16 @@ const config: Config = {
   },
   plugins: [animate, typography],
 };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 
 export default config;
