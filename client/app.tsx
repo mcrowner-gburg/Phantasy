0d2a56e5061eaca8387ae7f4c4bd82f16ff2700b
mcrowner-gburg <mcrowner@barkingcatmediagroup.com>
2025-11-21 15:39:29 -0500

    Create app.tsx

---
+++ client/app.tsx
@@ -0,0 +1,13 @@
+// client/src/App.tsx
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Phantasy!</h1>
+      <p>Your site is running and styled correctly.</p>
+    </div>
+  );
+};
+
+export default App;
