3714c5cddd8315e1f79173f5ef94077572f5e4e7
mcrowner-gburg <mcrowner@barkingcatmediagroup.com>
2025-11-21 15:54:23 -0500

    Update main.tsx

--- client/main.tsx
+++ client/main.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css"; // your global styles
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
