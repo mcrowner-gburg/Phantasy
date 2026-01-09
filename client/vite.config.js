1b60fa8349f0d266fc0b821377fbf00ac3ff67f2
mcrowner-gburg <mcrowner@barkingcatmediagroup.com>
2026-01-09 09:55:42 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
-  base: "./", // Make assets load relative to index.html
   plugins: [react()],
+  base: "./",       // relative paths for JS/CSS assets
   build: {
-    // Output directly to server/dist/client
-    outDir: path.resolve(__dirname, "../server/dist/client"),
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        entryFileNames: "assets/[name]-[hash].js",
-        chunkFileNames: "assets/[name]-[hash].js",
-        assetFileNames: "assets/[name]-[hash].[ext]",
-      },
-    },
-  },
-  resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    outDir: "dist", // already your default
   },
 });
