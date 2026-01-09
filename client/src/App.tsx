027831ef02d8133dd64b11fcac92e0936e0c1102
Matt Crowner <mcrowner@getbenefitsadmin.com>
2026-01-09 13:33:04 -0500

    fix jsx in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,8 @@ import React from "react";
 function App() {
   return (
     <div>
-<h1>Welcome to Phantasy!</h1>
+      <h1>Welcome to Phantasy!</h1>
       <p>Draft your favorite Phish songs and score points!</p>
-      </div>
       <h1>Hello Phantasy!</h1>
     </div>
   );
