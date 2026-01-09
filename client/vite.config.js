e714834a38a069dd181c35986023d28cc73f18be
Matt Crowner <mcrowner@getbenefitsadmin.com>
2026-01-09 11:13:52 -0500

    Add production React build

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",  // makes JS/CSS paths relative
+  base: "./",        // makes JS/CSS paths relative
   build: {
-    outDir: "dist",
+    outDir: "dist",   // output folder for production build
+    assetsDir: "assets", // place JS/CSS in dist/assets
   },
 });
