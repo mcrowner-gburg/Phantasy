d2969bd781b5d1188c2f00e160e0bfd216ce3b77
Matt Crowner <mcrowner@getbenefitsadmin.com>
2026-01-09 13:26:47 -0500

    Update App.tsx with local changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,9 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1>Hello Phantasy!</h1>
-      <p>Your React app is now running.</p>
-    </div>
+<h1>Welcome to Phantasy!</h1>
+      <p>Draft your favorite Phish songs and score points!</p>
+      </div>
   );
 }
 
