cd8ad3e86432c4a31c4ff5e8ba7ac31594087208
mcrowner-gburg <mcrowner@barkingcatmediagroup.com>
2026-01-09 11:05:15 -0500

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.css"; // this must exist (can be empty at first)
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<App />);
