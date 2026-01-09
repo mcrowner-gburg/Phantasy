813aa000bda1354f99948b45860006dbf9e39fac
Matt Crowner <mcrowner@getbenefitsadmin.com>
2026-01-09 14:34:38 -0500

    fix vite config and add typescript configs for path aliases

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",        // makes JS/CSS paths relative
+  base: "./",
   build: {
-    outDir: "dist",   // output folder for production build
-    assetsDir: "assets", // place JS/CSS in dist/assets
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
