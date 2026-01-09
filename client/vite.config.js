96e571c453a6b46cec14c6b8ce33be250bfc4c26
mcrowner-gburg <mcrowner@barkingcatmediagroup.com>
2026-01-09 09:59:23 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",       // relative paths for JS/CSS assets
+  base: "./",  // makes JS/CSS paths relative
   build: {
-    outDir: "dist", // already your default
+    outDir: "dist",
   },
 });
